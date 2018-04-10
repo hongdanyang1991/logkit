fe10a70d664acdc6df24ca23ac2da582a0781f33
ll <925783178@qq.com>
2018-04-10 09:30:25 +0800

    更换ip数据库

--- transforms/ip/ip.go
+++ transforms/ip/ip.go
@@ -8,11 +8,12 @@ import (
 	"github.com/qiniu/logkit/utils"
 	. "github.com/qiniu/logkit/utils/models"
 
-	"github.com/wangtuanjie/ip17mon"
 	"strconv"
 	"math/rand"
 	"time"
 	"strings"
+	"github.com/oschwald/geoip2-golang"
+	"net"
 )
 
 //更全的免费数据可以在ipip.net下载
@@ -20,8 +21,17 @@ type IpTransformer struct {
 	StageTime string `json:"stage"`
 	Key       string `json:"key"`
 	DataPath  string `json:"data_path"`
-	loc       *ip17mon.Locator
+	db        *geoip2.Reader
 	stats     utils.StatsInfo
+}
+
+func (it *IpTransformer) Init() error {
+	db, err := geoip2.Open(it.DataPath)
+	if err != nil {
+		return err
+	}
+	it.db = db
+	return nil
 }
 
 func (it *IpTransformer) RawTransform(datas []string) ([]string, error) {
@@ -97,6 +107,62 @@ func (it *IpTransformer) randomIp(doc Data) {
 
 func (it *IpTransformer) Transform(datas []Data) ([]Data, error) {
 	var err, ferr error
+	errnums := 0
+	keys := utils.GetKeys(it.Key)
+	newkeys := make([]string, len(keys))
+	for i := range datas {
+
+		//工业互联网 内置
+		if _, ok := datas[i][it.Key]; ok {
+			it.randomIp(datas[i])
+		}
+
+		copy(newkeys, keys)
+		val, gerr := utils.GetMapValue(datas[i], keys...)
+		if gerr != nil {
+			errnums++
+			err = fmt.Errorf("transform key %v not exist in data", it.Key)
+			continue
+		}
+		strval, ok := val.(string)
+		if !ok {
+			errnums++
+			err = fmt.Errorf("transform key %v data type is not string", it.Key)
+			continue
+		}
+
+		record, nerr := it.db.City(net.ParseIP(strval))
+		if nerr != nil {
+			err = nerr
+			errnums++
+			continue
+		}
+		newkeys[len(newkeys)-1] = "City"
+		utils.SetMapValue(datas[i], record.City.Names["zh-CN"], false, newkeys...)
+		Subdivisions := ""
+		if len(record.Subdivisions) > 0 {
+			Subdivisions = record.Subdivisions[0].Names["zh-CN"]
+		}
+		newkeys[len(newkeys)-1] = "Region"
+		utils.SetMapValue(datas[i], Subdivisions, false, newkeys...)
+		newkeys[len(newkeys)-1] = "Country"
+		utils.SetMapValue(datas[i], record.Country.Names["zh-CN"], false, newkeys...)
+		newkeys[len(newkeys)-1] = "Latitude"
+		utils.SetMapValue(datas[i], record.Location.Latitude, false, newkeys...)
+		newkeys[len(newkeys)-1] = "Longitude"
+		utils.SetMapValue(datas[i], record.Location.Longitude, false, newkeys...)
+	}
+	if err != nil {
+		it.stats.LastError = err.Error()
+		ferr = fmt.Errorf("find total %v erorrs in transform IP, last error info is %v", errnums, err)
+	}
+	it.stats.Errors += int64(errnums)
+	it.stats.Success += int64(len(datas) - errnums)
+	return datas, ferr
+}
+
+/*func (it *IpTransformer) Transform(datas []Data) ([]Data, error) {
+	var err, ferr error
 	if it.loc == nil {
 		it.loc, err = ip17mon.NewLocator(it.DataPath)
 		if err != nil {
@@ -148,7 +214,7 @@ func (it *IpTransformer) Transform(datas []Data) ([]Data, error) {
 	it.stats.Errors += int64(errnums)
 	it.stats.Success += int64(len(datas) - errnums)
 	return datas, ferr
-}
+}*/
 
 func (it *IpTransformer) Description() string {
 	//return "transform ip to country region and isp"
