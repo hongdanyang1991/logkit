b9ba0222b433c8170564f0dda28bc5c591024ced
ll <925783178@qq.com>
2018-05-04 09:50:01 +0800

    删除无用测试

--- plugin/plugin_test.go
+++ plugin/plugin_test.go
@@ -1,7 +1,6 @@
 package plugin
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -9,18 +8,7 @@ func TestListPlugins(t *testing.T) {
 	Conf = &Config{
 		Enabled: true,
 		Dir:     "E:\\Go\\GOPATH\\src\\github.com\\qiniu\\logkit\\plugins",
-		Git:     "",
+		RemoteSource:     "",
 	}
 	SyncPlugins()
-}
-
-func TestPluginRun(t *testing.T) {
-	plugin := &Plugin{
-		Path:         "E:\\Go\\GOPATH\\src\\github.com\\qiniu\\logkit\\plugins\\hello",
-		MTime:        1000000,
-		DefaultCycle: 10,
-		ExecFile:     "10_hello.exe",
-	}
-	metric := PluginRun(plugin, "a")
-	fmt.Println(metric)
 }
